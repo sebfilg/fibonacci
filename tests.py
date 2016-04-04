6d2df7cfc8d34bfa2325d2a8ea4ce20223aced56
John Hess <mgojohn@gmail.com>
2016-04-03 20:11:24 -0400

    sweet recursive fibonacci calculator with a test

---
+++ tests.py
@@ -0,0 +1,11 @@
+# tests.py
+from fibonacci import fib
+
+def test_fib_output():
+    assert fib(0) == 0
+    assert fib(1) == 1
+    assert fib(2) == 1
+    assert fib(3) == 2
+    assert fib(4) == 3
+    assert fib(5) == 5
+
