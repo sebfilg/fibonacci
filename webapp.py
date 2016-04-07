1e5e67674647e9f1481a3155a29081bd971cb502
John Hess <mgojohn@gmail.com>
2016-04-06 22:26:02 -0400

    awesome fibonacci number calculator

--- webapp.py
+++ webapp.py
@@ -1,10 +1,19 @@
 # webapp.py
-from flask import Flask
+from flask import Flask, render_template, request
+from fibonacci import fib
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def welcome():
+    return render_template('welcome.html')
+
+@app.route('/fib')
+def calculate_fib():
+    which_number = int(request.args.get('fib_number'))
+    return "Fibonacci number #{} is {}".format(
+        which_number,
+        str(fib(which_number))
+    )
 
 if __name__ == '__main__':
     app.run()
